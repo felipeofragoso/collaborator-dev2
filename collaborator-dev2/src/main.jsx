95fe35602c34d91b7f3af19d91eb69a9a6370c6d
felipeofragoso <lfo.fragoso@gmail.com>
2024-06-16 23:47:19 -0300

    Teste Layout

--- collaborator-dev2/src/main.jsx
+++ collaborator-dev2/src/main.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import App from './App'; 
 import './index.css'
 export default Home;
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 import PageNotFound from './pages/PageNotFound/PageNotFound';
 import Home from './pages/Home/Home';
+import { Layout } from 'antd';
 const router = createBrowserRouter([
   {
     path: "/",
@@ -17,6 +18,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+ <App >
     <RouterProvider router={router} />
+  </App>
+    {/* <RouterProvider router={router} /> */}
   </React.StrictMode>,
 )
